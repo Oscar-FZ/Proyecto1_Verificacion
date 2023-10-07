fac5a58f16f761e172b220feab5b3a371b49cefa
EmaBR23 <79341113+EmaBR23@users.noreply.github.com>
2023-10-06 22:04:13 -0600

    Comment score_board.sv

--- score_board.sv
+++ score_board.sv
@@ -1,12 +1,13 @@
 class score_board #(parameter drvrs = 4, parameter pckg_sz = 16);
-    bus_pckg_mbx mntr_sb_mbx;
-    bus pckg_mbx drvr_sb_mbx;
-    sb_pckg #(.drvrs(drvrs), .pckg_sz(pckg_sz)) transaccion_entrante;
-    sb_pckg scoreboard [$];
+	//Mailbox
+    bus_pckg_mbx mntr_sb_mbx; //Recibir paquetes del monitor
+    bus pckg_mbx drvr_sb_mbx; //Enviar a los dispositivos conectados al bus
+    sb_pckg #(.drvrs(drvrs), .pckg_sz(pckg_sz)) transaccion_entrante; //Almacenar un paquete
+    sb_pckg scoreboard [$]; //Almacenar un historial de paquetes
     sb_pckg auxiliar_array[$];
 
-    shortreal retardo_promedio;
-    solicitud_sb orden;
+    shortreal retardo_promedio; //Almacenar el retardo promedo de todas las trasacciones
+    solicitud_sb orden; //Almacena una orden recibida de la unidad de control
     int size_sb = 0;
     int transacciones_completadas = 0;
     int retardo_total = 0;
@@ -15,14 +16,21 @@ class score_board #(parameter drvrs = 4, parameter pckg_sz = 16);
 	$display("[%g] El Score Board ha iniciado", $time);
 
 	forever begin
-	    if (mntr_sb_mbx.num()>0) begin
-		mntr_sb_mbx.get(transaccion_entrante);
+	    if (mntr_sb_mbx.num()>0) begin //Verifica si hay paquetes disponibles en el mailbox  mntr_sb_mbx
+		mntr_sb_mbx.get(transaccion_entrante); // obtiene un paquete de el mailbox y lo almacena en la variable transaccion_entrante
 		transaccion_entrante.print("[SCORE BOARD] Transaccion recibida desde el Monitor");
+
+		// Cambio sugerido: Agregar más información al reporte de la consola
+		$display("Origen: %d, Destino: %d, Tipo: %d, Latencia: %d",
+			transaccion_entrante.Origen,
+			transaccion_entrante.Destino,
+			transaccion_entrante.Tipo,
+			transaccion_entrante.latencia);
 
 	    end
 	end
     endtask
 
 
-
 endclass
+
